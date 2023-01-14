108447b9aec9e7a31ad798dade46269fbe598d07
root <insac96@gmail.com>
2023-01-14 02:54:10 +0000

    Update

--- _client/game/main.js
+++ _client/game/main.js
@@ -14,13 +14,6 @@ function launch(parentID) {
       width: 1920,
       height: 1080
     },
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 300 },
-        debug: false
-      }
-    },
     scene: [
       BootScene, PlayScene
     ]
--- _client/game/scenes/play.js
+++ _client/game/scenes/play.js
@@ -7,29 +7,136 @@ export default class PlayScene extends Scene {
     }
 
     create () {
-        const Map = this.createMap()
+        this.Map = this.createMap() 
+
+        for (let i = 0; i < 20; i++) {
+            this.Player = this.createPlayer()
+            this.Player.SetData('shoot', 2)
+            this.Player.SetData('attack-speed', Phaser.Math.Between(300, 1200))
+            this.Player.SetData('attack-delay', Phaser.Math.Between(300, 1500))
+        }
     }
 
-    update () {
-
-    }
-
-    createMap () {
-        const Key = Math.floor((Math.random() * 4) + 1)
+    createMap (key) {
+        const Key = key ? key : Math.floor((Math.random() * 4) + 1)
         const Map = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, `Map_${Key}`)
         Map.setScale(Math.max(this.cameras.main.width / Map.width, this.cameras.main.height / Map.height)).setScrollFactor(0) 
         return Map
     }
 
-    createPlayer () {
-        const Player = this.add.group()
-        const Key = Math.floor((Math.random() * 3) + 1)
+    createPlayer (key) {
+        // Get Key
+        const Key = key ? key : Math.floor((Math.random() * 3) + 1)
 
-        const Player_Idle = this.add.sprite(0, 0, `Player_${Key}_Idle`)
-        //const Player_Jump = this.add.sprite(0, 0, `Player_${Key}_Jump`)
-        //const Player_Run = this.add.sprite(0, 0, `Player_${Key}_Run`)
-        //const Player_Sitdown = this.add.sprite(0, 0, `Player_${Key}_Sitdown`)
-        //const Player_Walk = this.add.sprite(0, 0, `Player_${Key}_Walk`)
+        // Player Group
+        const Player = this.add.container()
+        Player.setPosition(400,500)
+        Player.setScale(4)
+
+        // Create Character Asset
+        const Character = this.add.sprite(0, 0, `Player_${Key}`)
+        Character.setOrigin(0)
+        Character.name = 'Character'
+        Character.anims.create({key: 'idle', frames: `Player_${Key}_Idle`, frameRate: 15, repeat: -1})
+        Character.anims.create({key: 'run', frames: `Player_${Key}_Run`, frameRate: 15, repeat: -1})
+        Character.anims.create({key: 'walk', frames: `Player_${Key}_Walk`, frameRate: 15, repeat: -1})
+        Character.play('idle')
+        Character.setDataEnabled()
+        Character.data.set('name', 'Player')
+        Character.data.set('health', 1000)
+        Character.data.set('main-health', 1000)
+        Character.data.set('shoot', 1)
+        Character.data.set('dame', 10)
+        Character.data.set('attack-speed', 1200)
+        Character.data.set('attack-delay', 1500)
+        Player.add(Character)
+
+        // Create Health Bar Asset
+        const HealthBar = this.add.graphics()
+        HealthBar.name = 'HealthBar'
+        HealthBar.fillStyle(0x2ecc71, 1)
+        HealthBar.fillRect(0, 5, Character.width / 2, 3)
+        HealthBar.setPosition((0 - (Character.width / 4) + (Character.width / 4)), 0)
+        Player.add(HealthBar)
+
+        // Create Name Text
+        const NameText = this.add.text(0, 0, Character.data.get('name'), {
+            font: 'bold 8px Arial', 
+            fill: '#fff',
+        })
+        NameText.setPosition((0 - (NameText.width / 2) + (Character.width / 4)), -6)
+        Player.add(NameText)
+
+        // Create Shoot Asset
+        const Shoot = this.add.sprite(0, 0, `Shoot_${Character.data.get('shoot')}`)
+        Shoot.setOrigin(0)
+        Shoot.setVisible(false)
+
+        // Fire
+        function Fire () {
+            Shoot.setPosition(Player.x + Character.width, Player.y + Character.height)
+            Shoot.setVisible(true)
+
+            this.tweens.add({
+                targets: Shoot,
+                x: this.cameras.main.width,
+                duration: Character.data.get('attack-speed'),
+                ease: 'Power1',
+                onComplete: function (tween, targets) {
+                    targets[0].setVisible(false)
+                    Shoot.setPosition(Player.x + Character.width, Player.y + Character.height)
+                }
+            })
+
+            this.time.addEvent({ delay: Character.data.get('attack-delay'), callback: Fire, callbackScope: this })
+        }
+
+        // Run
+        function Run () {
+            Character.play('run')
+            this.tweens.add({
+                targets: Player,
+                x: Phaser.Math.Between(50, (this.cameras.main.width / 2 - 200)),
+                y: Phaser.Math.Between(250, (this.cameras.main.height - 250)),
+                duration: Phaser.Math.Between(200, 1000),
+                ease: 'Linear',
+                onComplete: function () {
+                    Character.play('idle')
+                }
+            })
+
+            this.time.addEvent({ delay: Phaser.Math.Between(1000, 4000), callback: Run, callbackScope: this })
+        }
+
+        // Set Health Player
+        Player.SetHealth = function (value) {
+            Character.data.set('health', Character.data.get('main-health') + value)
+            HealthBar.scaleX = value / Character.data.get('main-health')
+        }
+
+        // Get Data Player
+        Player.GetData = function () {
+            return {
+                health: Character.data.get('health'),
+                mainHealth: Character.data.get('main-health'),
+                shoot: Character.data.get('shoot'),
+                dame: Character.data.get('dame'),
+                attackSpeed: Character.data.get('attack-speed'),
+                attackDelay: Character.data.get('attack-delay'),
+            }
+        }
+
+        // Set Data Player
+        Player.SetData = function (key, value) {
+            Character.data.set(key, value)
+        }
+
+        // Loop
+        this.time.addEvent({ delay: 0, callback: Fire, callbackScope: this })
+        this.time.addEvent({ delay: 0, callback: Run, callbackScope: this })
+
+        // Return
+        return Player
     }
 }
 
